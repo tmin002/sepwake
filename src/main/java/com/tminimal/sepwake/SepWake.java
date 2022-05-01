25283e2994a6b063a3b83f7245a0f6a636f53d86
tmin002 <ssh9930@gmail.com>
2022-05-01 16:05:58 +0900

    Implement WindowsTTS

--- src/main/java/com/tminimal/sepwake/SepWake.java
+++ src/main/java/com/tminimal/sepwake/SepWake.java
@@ -5,21 +5,14 @@ import com.tminimal.sepwake.config.ConfigLoader;
 import com.tminimal.sepwake.config.ConfigSaver;
 import com.tminimal.sepwake.gui.msgbox.MsgBox;
 import com.tminimal.sepwake.gui.wnd.MainWnd;
-import com.tminimal.sepwake.gui.wnd.Window;
 import com.tminimal.sepwake.ring.RingManager;
 import com.tminimal.sepwake.ring.alarmScript;
 import com.tminimal.sepwake.tts.MacosTTS;
 import com.tminimal.sepwake.tts.TTS;
-import jdk.nashorn.internal.runtime.regexp.joni.Config;
+import com.tminimal.sepwake.tts.WindowsTTS;
+import org.omg.CORBA.Environment;
 
-import javax.swing.*;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.Map;
 
 public class SepWake {
 
@@ -28,6 +21,7 @@ public class SepWake {
 
     public static void main(String[] args){
         try {
+            MsgBox.show(SEPWAKE_PWD);
             setup();
             MainWnd w = new MainWnd();
 
@@ -45,6 +39,8 @@ public class SepWake {
         OSdetect.OStype type = OSdetect.detectOS();
         if (type == OSdetect.OStype.macOS) {
             defaultTTS = new MacosTTS();
+        } else if (type == OSdetect.OStype.Windows) {
+            defaultTTS = new WindowsTTS();
         } else {
             MsgBox.show("This environment is not supported yet. (macOS only)");
             System.exit(1);
--- src/main/java/com/tminimal/sepwake/tts/WindowsTTS.java
+++ src/main/java/com/tminimal/sepwake/tts/WindowsTTS.java
@@ -1,10 +1,37 @@
 package com.tminimal.sepwake.tts;
 
+import com.tminimal.sepwake.d;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WindowsTTS extends TTS{
+
+    private static final String SPEAKER_SCRIPT = "CreateObject(\"SAPI.spVoice\").Speak(Wscript.Arguments(1))";
+    private static final String SPEAKER_SCRIPT_PATH = ".\\sapi.vbs";
     @Override
     public void speak(String str) {
-
+        try {
+            createSpeaker();
+            Process say = Runtime.getRuntime().exec(new String[] {"cscript", SPEAKER_SCRIPT_PATH, "/nologo", str});
+            try {
+                say.waitFor();
+            } catch (InterruptedException e) {
+                say.destroy();
+            }
+        } catch (IOException e) {
+            d.s("Failed to initialize TTS.");
+        }
     }
-    //TODO: WindowsTTS implementation
+
+    private void createSpeaker() throws IOException {
+        File speaker = new File(SPEAKER_SCRIPT_PATH);
+        if (!speaker.isFile()) {
+            FileWriter writer = new FileWriter(speaker);
+            writer.write(SPEAKER_SCRIPT);
+            writer.close();
+        }
+    }
 
 }
